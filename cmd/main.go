3c28c373c03da4dba2fe07c882e39fe518e93625
Luidy <ejjo321@gmail.com>
2021-04-20 20:10:14 +0900

    add. unary interceptor

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,35 @@ package main
 import (
 	"alro/config"
 	"alro/server"
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"net"
+	"os"
 )
+var log *logrus.Logger
 
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
-	//log := logrus.StandardLogger()
+	log = logrus.StandardLogger()
 	alro := config.Alro
-	server.NewGRPCServer(alro)
+	g := server.NewGRPCServer(alro)
+
+	startServer(alro, g)
+}
+
+func startServer(alro *viper.Viper, g *grpc.Server) {
+	address := fmt.Sprintf("0.0.0.0:%d", alro.GetInt("port"))
+	fmt.Printf("start ALRO!!!")
+
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Println("End to bind for gRPC server", "err", err)
+		os.Exit(1)
+	}
+	if err := g.Serve(l); err != nil {
+		log.Println("End gRPC server", "err", err)
+		os.Exit(1)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -2,15 +2,30 @@ package server
 
 import (
 	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"net"
 	"os"
+	"time"
 )
 
 func NewGRPCServer(alro *viper.Viper) (g *grpc.Server) {
-	g = grpc.NewServer()
+	g = grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_ctxtags.UnaryServerInterceptor(
+				grpc_ctxtags.WithFieldExtractor(
+					grpc_ctxtags.CodeGenRequestFieldExtractor,
+				),
+			),
+		),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionAge: 10 * time.Second,
+		}),
+	)
 	address := fmt.Sprintf("0.0.0.0:%d", alro.GetInt("port"))
 	fmt.Printf(address)
 
